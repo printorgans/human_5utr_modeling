a112bcfb23ad2e0b3c0bdea32576586651dfa79f
rfuisz <richardfuisz@gmail.com>
2024-07-10 17:53:25 -0700

    good

--- design_5_utr.py
+++ design_5_utr.py
@@ -6,6 +6,10 @@ import seaborn as sns
 from sklearn import preprocessing
 from keras.models import load_model
 import h5py
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv1D, Flatten, Dense, Dropout, Activation
+from tensorflow.keras.initializers import VarianceScaling, Zeros
+
 
 # import keras
 # from keras.preprocessing import sequence
@@ -97,32 +101,299 @@ scaler.fit(scale_utrs['rl'].values.reshape(-1, 1))
 
 # model = load_model('./modeling/saved_models/retrained_main_MRL_model.h5')
 # Load the model
-try:
-    model = load_model('./modeling/saved_models/retrained_evolution_model.hdf5')
-except TypeError as e:
-    print(f"Error occurred: {e}")
-    with h5py.File('./modeling/saved_models/retrained_evolution_model.hdf5', 'r') as f:
-        config = f.attrs.get('model_config')
-        print(f"model_config: {config}")
-        # Attempt to decode the config if it's in bytes
-        if isinstance(config, bytes):
-            config = config.decode('utf-8')
-        print(f"Decoded model_config: {config}")
-        # Parse the JSON configuration to inspect it
-        import json
-        config_dict = json.loads(config)
-        print(f"config_dict: {config_dict}")
+def load_attempt():
+    try:
+        model = load_model('./modeling/saved_models/retrained_evolution_model.hdf5')
+    except TypeError as e:
+        print(f"Error occurred: {e}")
+        ## print trace:
+        import traceback
+        traceback.print_exc()
+        with h5py.File('./modeling/saved_models/retrained_evolution_model.hdf5', 'r') as f:
+            config = f.attrs.get('model_config')
+            print(f"model_config: {config}")
+            # Attempt to decode the config if it's in bytes
+            if isinstance(config, bytes):
+                config = config.decode('utf-8')
+            print(f"Decoded model_config: {config}")
+            # Parse the JSON configuration to inspect it
+            import json
+            config_dict = json.loads(config)
+            print(f"config_dict: {config_dict}")
 
-        if "config" in config_dict:
-            config_list = config_dict["config"]
-            print(f"config['config']: {config_list}")
-            print(f"Type of config['config']: {type(config_list)}")
+            if "config" in config_dict:
+                config_list = config_dict["config"]
+                print(f"config['config']: {config_list}")
+                print(f"Type of config['config']: {type(config_list)}")
 
-            # If you need to remove an item from the list, use indexing
-            batch_input_shape = None
-            for item in config_list:
-                if isinstance(item, dict) and "batch_input_shape" in item:
-                    batch_input_shape = item.pop("batch_input_shape")
-                    break
+                # If you need to remove an item from the list, use indexing
+                batch_input_shape = None
+                for item in config_list:
+                    if isinstance(item, dict) and "batch_input_shape" in item:
+                        batch_input_shape = item.pop("batch_input_shape")
+                        break
 
-            print(f"batch_input_shape: {batch_input_shape}")
+                print(f"batch_input_shape: {batch_input_shape}")
+
+
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv1D, Flatten, Dense, Dropout, Activation, Input
+from tensorflow.keras.initializers import VarianceScaling, Zeros
+
+class ModelWithWeights:
+    def __init__(self):
+        self.model = self.build_model()
+
+    def build_model(self):
+        model = Sequential()
+
+        model.add(Input(shape=(54, 4)))
+
+        model.add(Conv1D(
+            filters=40, 
+            kernel_size=8, 
+            strides=1, 
+            padding='valid', 
+            activation='relu', 
+            kernel_initializer=VarianceScaling(scale=1.0, mode='fan_avg', distribution='uniform'),
+            bias_initializer=Zeros()
+        ))
+
+        model.add(Conv1D(
+            filters=40, 
+            kernel_size=8, 
+            strides=1, 
+            padding='valid', 
+            activation='relu', 
+            kernel_initializer=VarianceScaling(scale=1.0, mode='fan_avg', distribution='uniform'),
+            bias_initializer=Zeros()
+        ))
+
+        model.add(Flatten())
+
+        model.add(Dense(
+            units=40, 
+            activation='linear', 
+            kernel_initializer=VarianceScaling(scale=1.0, mode='fan_avg', distribution='uniform'),
+            bias_initializer=Zeros()
+        ))
+
+        model.add(Dropout(rate=0.2))
+
+        model.add(Activation('relu'))
+
+        model.add(Dense(
+            units=1, 
+            activation='linear', 
+            kernel_initializer=VarianceScaling(scale=1.0, mode='fan_avg', distribution='uniform'),
+            bias_initializer=Zeros()
+        ))
+
+        model.add(Activation('linear'))
+
+        return model
+
+    def load_weights(self, hdf5_path):
+        self.model.load_weights(hdf5_path)
+
+
+
+import random
+import math
+
+def ret_rand_nuc():
+    x = random.randint(0,3)
+    if x == 0:
+        return [1,0,0,0] # A
+    if x == 1:
+        return [0,1,0,0] # C
+    if x == 2:
+        return [0,0,1,0] # G
+    if x == 3:
+        return [0,0,0,1] # T
+    
+def vector_to_nuc(arr, seq_len=50):
+    seq = ''
+    for i in range(seq_len):
+        if arr[i,0] == 1:
+            seq = seq + 'A'
+        if arr[i,1] == 1:
+            seq = seq + 'C'
+        if arr[i,2] == 1:
+            seq = seq + 'G'
+        if arr[i,3] == 1:
+            seq = seq + 'T'
+    return seq
+
+def convert_and_save(sequences, predictions):
+    # Convert the one-hot encoded sequences to A, C, T, G
+    seqs = []
+    for nbr in xrange(len(sequences)):
+        seqs.append(vector_to_nuc(sequences[nbr]))
+    df = pd.DataFrame(data=[seqs,predictions.tolist()]).transpose()
+    df.columns = ['utr', 'prediction']
+    df.sort_values('prediction', ascending=False, inplace=True)
+    return df
+
+def make_random_sequences(nbr_sequences, length, constant='', no_uaug=False, no_stop=False):
+    # Make randomized sequences, allowing for the inclusion / exclusion of uATGs / stop codons
+    seqs = []
+    nucs = {0:'A', 1:'T', 2:'C', 3:'G'}
+    i = 0
+    while i < nbr_sequences:
+        new_seq = ''
+        for n in range(length - len(constant)):
+            new_seq = new_seq + nucs[random.randint(0,3)]
+        
+        if no_uaug == False or (no_uaug==True and 'ATG' not in new_seq):
+            if no_stop == False or (no_stop == True and ('TAG' not in new_seq and 'TGA' not in new_seq and 'TAA' not in new_seq)):
+                new_seq = new_seq + constant
+                seqs.append(new_seq)
+                i+=1
+    return seqs
+
+def simple_mutate(seq, nbr_bases=1, prob=1):
+    if nbr_bases > 1 and prob > random.random():
+        nbr_bases = nbr_bases
+    else:
+        nbr_bases = 1
+    for i in range(nbr_bases):
+        pos = random.randint(0, 49)
+        seq[pos] = ret_rand_nuc()
+    return seq
+
+def check_for_uaug(seq):
+    seq = vector_to_nuc(seq)
+    return 'ATG' in seq[:50]
+
+def check_for_stops(seq):
+    seq = vector_to_nuc(seq)
+    if 'TAG' in seq[:50] or 'TGA' in seq[:50] or 'TAA' in seq[:50]:
+        return True
+    return False
+
+def negative_selection(seq, model, scaler, target_val, no_uaug=False, no_stop=False, nbr_bases_to_mutate=1, multi_mutate_prob=1):
+    seqs = np.empty([2,54,4])
+    seqs[0] = seq.copy()
+    seqs[1] = simple_mutate(seq.copy(), nbr_bases=nbr_bases_to_mutate, prob=multi_mutate_prob)
+    
+    if no_uaug == True and check_for_uaug(seqs[1]):
+        return seqs[0]
+    if no_stop == True and check_for_stops(seqs[1]):
+        return seqs[0]
+    
+    scores = model.predict(seqs).reshape(-1)
+    scores = scaler.inverse_transform(scores)
+    if scores[1] < scores[0]:
+        if scores[1] >= target_val:
+            return seqs[1]
+        else:
+            return seqs[0]
+    else:
+        return seqs[0]    
+
+def selection(seq, model, scaler, target_val, no_uaug=False, no_stop=False, nbr_bases_to_mutate=1, multi_mutate_prob=1):
+    seqs = np.empty([2,50,4])
+    seqs[0] = seq.copy()
+    seqs[1] = simple_mutate(seq.copy(), nbr_bases=nbr_bases_to_mutate, prob=multi_mutate_prob)
+    
+    if no_uaug == True and check_for_uaug(seqs[1]):
+        return seqs[0]
+    if no_stop == True and check_for_stops(seqs[1]):
+        return seqs[0]
+    
+    scores = model.predict(seqs).reshape(-1)
+    scores = scaler.inverse_transform(scores)
+    if scores[1] > scores[0]:
+        if scores[1] <= target_val:
+            return seqs[1]
+        else:
+            return seqs[0]
+    else:
+        return seqs[0]    
+    
+def selection_to_target(seq, model, scaler, target_val, no_uaug=False, no_stop=False, nbr_bases_to_mutate=1, multi_mutate_prob=1, seq_len=50, accept_range=0.1):
+    seqs = np.empty([2,seq_len,4])
+    # Save the incoming sequence before mutating
+    seqs[0] = seq.copy()
+    # The mutated sequence
+    seqs[1] = simple_mutate(seq.copy(), nbr_bases=nbr_bases_to_mutate, prob=multi_mutate_prob)
+    
+    # Decide whether to continue with the new sequence based on the uAUG / stop codon preference
+    if no_uaug == True and check_for_uaug(seqs[1]):
+        return seqs[0]
+    if no_stop == True and check_for_stops(seqs[1]):
+        return seqs[0]
+    
+    scores = model.predict(seqs).reshape(-1)
+    scores = scaler.inverse_transform(scores)
+    
+    # Accept sequences that fall within this range. May provide more sequence diversity
+    if scores[0] >= target_val - accept_range and scores[0] <= target_val + accept_range:
+        return seqs[0]
+    else:
+        if abs(target_val - scores[1]) <= abs(target_val - scores[0]):
+            return seqs[1]
+        else:
+            return seqs[0]    
+        
+
+# Usage
+model_with_weights = ModelWithWeights()
+model_with_weights.load_weights('./modeling/saved_models/retrained_evolution_model.hdf5')
+model_with_weights.model.summary()
+model = model_with_weights.model
+
+        
+# Dictionary where new sequences are saved
+evolved_seqs = {}
+
+# Number of evolution iterations
+iterations = 800
+# Number of bases to mutate if the probability to 'multi-mutate' is exceeded
+nbr_bases_to_mutate = 2
+# Probability to change multiple bases in an iteration
+prob_of_multi_mutation = 0.5
+# If using the original evolution model, set seq_len to 54. That model was
+# trained on UTRs that included the first for basees of the CDS (ATGG).
+seq_len = 50
+# Choose target MRLs and the number of sequences to create for each
+targets = [5, 8]
+seqs_per_target = [10, 10]
+# Choose whether or not to allow uAUGs and / or stop codons
+no_uaug = True
+no_stop = False
+
+for target_rl, nbr_sequences in zip(targets, seqs_per_target):
+    print('Working on target_rl {} with {} sequences:'.format(target_rl, nbr_sequences))
+    
+    # Randomly generate starting sequences for evolving
+    rand_seqs = make_random_sequences(nbr_sequences, seq_len, no_uaug=no_uaug, no_stop=no_stop)
+    test_sequences = np.empty([len(rand_seqs), seq_len, 4])
+    i = 0
+    
+    # One-hot encode sequences
+    for seq in rand_seqs:
+        test_sequences[i] = vectorizeSequence(seq.lower())
+        i += 1
+    
+    # Evolve sequences
+    for generation in range(0, iterations):
+        for i in range(len(test_sequences)):
+            test_sequences[i] = selection_to_target(seq=test_sequences[i], model=model, scaler=scaler, target_val=target_rl,no_uaug=no_uaug,
+                                        no_stop=no_stop, nbr_bases_to_mutate=nbr_bases_to_mutate, multi_mutate_prob=prob_of_multi_mutation, seq_len=seq_len)
+
+        if (generation + 1) %  20 == 0:
+            print('Generation: {}'.format(generation + 1))          
+    print
+    # Final prediction then convert to text sequence
+    predictions = model.predict(test_sequences).reshape(-1)
+    predictions = scaler.inverse_transform(predictions)
+    converted_df = convert_and_save(test_sequences,predictions)
+    
+    evolved_seqs[target_rl] = converted_df
+
+
+for i in evolved_seqs:
+    sns.kdeplot(evolved_seqs[i]['prediction'], shade=True, legend=True, label=i)
